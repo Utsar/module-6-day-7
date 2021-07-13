11d505a5dc579c62a2f5b35876702945d6714486
Kristian Utsar <78056935+Utsar@users.noreply.github.com>
2021-07-13 15:47:47 +0100

    crud boileplate

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express, { response } from "express";
 import listEndpoints from "express-list-endpoints";
 import cors from "cors";
+import blogsRouter from "../src/services/blogs/index.js";
 
 const server = express();
 const port = process.env.PORT || 5000;
@@ -8,3 +9,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 app.use(express.json());
+app.use("/blogs", blogsRouter);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+app.on("error", (error) =>
+  console.log("Server is not running due to an error:", error)
+);
